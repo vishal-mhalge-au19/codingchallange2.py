b5afcce5168534e5cc46aa2ac11ea4079e7c9e29
vishalmhalge <mhalge.vishal@gmail.com>
2021-06-11 01:56:55 +0530

    first commit

---
+++ codingchallange2.py
@@ -0,0 +1,10 @@
+a = input ("1")
+b = input ("2")
+c = input ("3")
+if a > b and a > c:
+    print(" a is maximum number")
+elif b > a and b > c:
+    print(" b is maximum number")
+else:
+    print("c is maximum number")    
+
